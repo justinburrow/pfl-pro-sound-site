c685dbe7b077f764e9323c5b790d4222a92ff860
Justin Burrow <justinburrow@gmail.com>
2025-12-14 14:45:34 -0800

    Installed Sanity + lenis

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,16 @@ import { webcore } from 'webcoreui/integration';
 import svelte from '@astrojs/svelte';
 import sanity from '@sanity/astro';
 
+import react from '@astrojs/react';
+
 //https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: 'f28fd9dk',
-      dataset: 'production',
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-    }),
-    svelte(),
-    webcore(),
-  ],
+  integrations: [sanity({
+    projectId: 'f28fd9dk',
+    dataset: 'production',
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+  }), svelte(), webcore(), react()],
   apiVersion: '2025-12-08',
   studioBasePath: '/content-studio',
   vite: {
