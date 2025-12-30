7bc0ef646e09a480b7d45612940a28f2f04b2ca0
Justin Burrow <justinburrow@gmail.com>
2025-12-30 01:00:01 -0700

    added new slider images and hardcoded direct references to relative files, changed technical services image on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,30 @@ import tailwindcss from '@tailwindcss/vite';
 import { webcore } from 'webcoreui/integration';
 import svelte from '@astrojs/svelte';
 import sanity from '@sanity/astro';
-
+import { loadEnv } from 'vite';
 import react from '@astrojs/react';
 
 //https://astro.build/config
 export default defineConfig({
-	integrations: [sanity({
-    projectId: 'f28fd9dk',
-    dataset: 'production',
-    // Set useCdn to false if you're building statically.
-    useCdn: false,
-  }), svelte(), webcore(), react()],
+  integrations: [
+    sanity({
+      projectId: 'f28fd9dk',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    }),
+    svelte(),
+    webcore(),
+    react(),
+  ],
   apiVersion: '2025-12-08',
   studioBasePath: '/content-studio',
   vite: {
     plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      //
+    },
   },
 });
